6312e3b04f66d3a64290dbc622734cd657efa98c
Connor Stauffer <cstauffer@buildzoom.com>
2015-11-15 18:52:01 -0800

    add sweet rocket ship to server notice

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port') + '  🚀');
 });
