8e9bae32950281cecf6f6b9dd0673a9c52f25d5d
Connor Stauffer <cstauffer@buildzoom.com>
2015-11-16 20:39:19 -0800

    update package

---
+++ app/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router';
+// import FooterStore from '../stores/FooterStore'
+// import FooterActions from '../actions/FooterActions';
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <footer>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-xs-12'>
+              <h3 className='lead'>Footer</h3>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
